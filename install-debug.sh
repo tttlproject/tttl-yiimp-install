51d8b2bbb8047a75c22b801e78841c5fb31d771a
boris9a9 <boris9a9@protonmail.com>
2021-07-27 11:13:59 +0700

    update source code

--- install-debug.sh
+++ install-debug.sh
@@ -372,7 +372,7 @@
     
     # Compil Blocknotify
     cd ~
-    git clone https://github.com/boris9a9/june-pool-yiimp
+    git clone https://github.com/junecoin-project/june-pool-yiimp.git
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     sudo make
--- install.sh
+++ install.sh
@@ -369,7 +369,7 @@
     
     # Compil Blocknotify
     cd ~
-    hide_output git clone https://github.com/boris9a9/june-pool-yiimp
+    hide_output git clone https://github.com/junecoin-project/june-pool-yiimp.git
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     hide_output sudo make
